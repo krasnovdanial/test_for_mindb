83479fb4ba78de3f6f6822c3bc697ffd9e7afb4b
Daniel Krasnov <58078456+krasnovdanial@users.noreply.github.com>
2023-09-16 12:15:27 +0300

    Update product.sql

--- product.sql
+++ product.sql
@@ -1,5 +1,21 @@
-SELECT Products.name [Имя продукта], Category.name [Имя категории]
-FROM Products 
-LEFT JOIN ProductsCategory  ON Products.id = ProductsCategory.products_id
-JOIN Category ON Category.id = ProductsCategory.category_id
-ORDER BY Products.name
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import broadcast, coalesce, collect_list
+
+spark = SparkSession.builder.getOrCreate()
+products = spark.createDataFrame([
+    (1, "Product A"),
+    (2, "Product B"),
+    (3, "Product C")
+], ["ProductID", "ProductName"])
+
+categories = spark.createDataFrame([
+    (1, "Category X"),
+    (2, "Category Y"),
+    (1, "Category Z")
+], ["ProductID", "CategoryName"])
+products = products.withColumnRenamed("ProductID", "ID").withColumnRenamed("ProductName", "Name")
+categories = categories.withColumnRenamed("ProductID", "ID").withColumnRenamed("CategoryName", "Name")
+result = products.join(broadcast(categories), on="ID", how="left")
+result = result.select("Name", coalesce("Name", "No Category").alias("CategoryName"))
+result = result.groupBy("Name").agg(collect_list("CategoryName").alias("CategoryNames"))
+result.show()
